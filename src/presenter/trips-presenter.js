cffcd3e4d9df67dfaaee2e344e45a8f31a4c0ffc
Ksenia <ksenechka.93@gmail.com>
2022-12-17 11:42:08 +0300

    feat: add empty list points message refactoring

--- src/presenter/trips-presenter.js
+++ src/presenter/trips-presenter.js
@@ -3,6 +3,7 @@ import EditPointView from '../view/edit-point-view.js';
 import NewPointView from '../view/add-new-point-view.js';
 import PointView from '../view/point-view.js';
 import PointListView from '../view/point-list-view.js';
+import ListEmptyView from '../view/list-empty-view.js';
 import {render, RenderPosition} from '../render.js';
 import { isEscapeKey } from '../util.js';
 
@@ -22,12 +23,23 @@ export default class TripPresenter {
   init() {
     this.#listPoints = [...this.#pointsModel.points];
 
+    this.#renderPointsList();
+  }
+
+  #renderPointsList() {
+    if (!this.#listPoints.length) {
+      render(new ListEmptyView(), this.#pointContainer);
+      return;
+    }
+
+
     render(new ListSortView(), this.#pointContainer);
     render(this.#pointListComponent, this.#pointContainer);
     render(new NewPointView(), this.#pointListComponent.element, RenderPosition.AFTERBEGIN);
 
     this.#listPoints.forEach((point) => this.#renderPoint(point));
   }
+
 
   #renderPoint(point) {
     const pointComponent = new PointView({point});
@@ -39,36 +51,32 @@ export default class TripPresenter {
 
     const replacePointToEditForm = () => {
       this.#pointListComponent.element.replaceChild(pointEditComponent.element, pointComponent.element);
+      editRollupBtn.addEventListener('click', onCloseEditPointForm);
+      editPointForm.addEventListener('submit', onCloseEditPointForm);
+      document.addEventListener('keydown', onEscKeyDown);
     };
+
     const replaceEditFormToPoint = () => {
       this.#pointListComponent.element.replaceChild(pointComponent.element, pointEditComponent.element);
+      editRollupBtn.removeEventListener('click', onCloseEditPointForm);
+      editPointForm.removeEventListener('submit', onCloseEditPointForm);
+      document.removeEventListener('keydown', onEscKeyDown);
     };
 
-    const onEscKeyDown = (evt) => {
+    function onEscKeyDown(evt) {
       if (isEscapeKey) {
-        evt.preventDefault();
-        replaceEditFormToPoint();
-        document.removeEventListener('keydown', onEscKeyDown);
+        onCloseEditPointForm(evt);
       }
-    };
+    }
 
-    editRollupBtn.addEventListener('click', (evt) => {
+    function onCloseEditPointForm (evt) {
       evt.preventDefault();
       replaceEditFormToPoint();
-      document.removeEventListener('keydown', onEscKeyDown);
-    });
+    }
 
     pointRollupBtn.addEventListener('click', () => {
       replacePointToEditForm();
-      document.addEventListener('keydown', onEscKeyDown);
     });
-
-    editPointForm.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-      replaceEditFormToPoint();
-      document.removeEventListener('keydown', onEscKeyDown);
-    });
-
 
     render(pointComponent, this.#pointListComponent.element);
   }
