bbd106d3bf538f69c7f99a7f283747f51620eb6c
Ksenia <ksenechka.93@gmail.com>
2023-01-09 18:58:28 +0300

    feat: add mode

--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -3,14 +3,24 @@ import EditPointView from '../view/edit-point-view.js';
 import { render, replace, remove } from '../framework/render.js';
 import { isEscapeKey } from '../util.js';
 
+const Mode = {
+  DEFAULT: 'DEFAULT',
+  EDITING: 'EDITING',
+};
+
 export default class PointPresenter {
   #pointsContainer = null;
   #pointComponent = null;
   #pointEditComponent = null;
-  #point = null;
+  #handleModeChange = null;
 
-  constructor({pointsContainer}) {
+  #point = null;
+  #mode = Mode.DEFAULT;
+
+
+  constructor({pointsContainer, onModeChange}) {
     this.#pointsContainer = pointsContainer;
+    this.#handleModeChange = onModeChange;
   }
 
   init(point) {
@@ -35,13 +45,11 @@ export default class PointPresenter {
       return;
     }
 
-    // Проверка на наличие в DOM необходима,
-    // чтобы не пытаться заменить то, что не было отрисовано
-    if (this.#pointsContainer.contains(prevPointComponent.element)) {
+    if (this.#mode === Mode.DEFAULT) {
       replace(this.#pointComponent, prevPointComponent);
     }
 
-    if (this.#pointsContainer.contains(prevPointEditComponent.element)) {
+    if (this.#mode === Mode.EDITING) {
       replace(this.#pointEditComponent, prevPointEditComponent);
     }
 
@@ -54,18 +62,27 @@ export default class PointPresenter {
     remove(this.#pointEditComponent);
   }
 
+  resetView() {
+    if (this.#mode !== Mode.DEFAULT) {
+      this.#replaceEditFormToPoint();
+    }
+  }
+
   #replacePointToEditForm() {
     replace(this.#pointEditComponent, this.#pointComponent);
     document.addEventListener('keydown', this.#escKeydownHandler);
+    this.#handleModeChange();
+    this.#mode = Mode.EDITING;
   }
 
   #replaceEditFormToPoint() {
     replace(this.#pointComponent, this.#pointEditComponent);
     document.removeEventListener('keydown', this.#escKeydownHandler);
+    this.#mode = Mode.DEFAULT;
   }
 
   #escKeydownHandler = (evt) => {
-    if (isEscapeKey) {
+    if (isEscapeKey(evt)) {
       evt.preventDefault();
       this.#replaceEditFormToPoint();
     }
--- src/util.js
+++ src/util.js
@@ -1,3 +1,7 @@
 const isEscapeKey = (evt) => (evt.key === 'Escape' || evt.key === 'Esc');
 
+// function updateItem(items, update) {
+//   return items.map((item) => item.id === update.id ? update : item);
+// }
+
 export {isEscapeKey};
